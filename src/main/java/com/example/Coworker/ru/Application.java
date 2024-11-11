1a1a1f2f29623636eab6e4379aa39a172cde7ba0
MaxKedroff <kedrov.maksim12345@gmail.com>
2024-11-11 16:01:14 +0500

    openApi docs added

--- src/main/java/com/example/Coworker/ru/Application.java
+++ src/main/java/com/example/Coworker/ru/Application.java
@@ -1,19 +1,15 @@
 package com.example.Coworker.ru;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
 	}
-
-
-
 }
 
